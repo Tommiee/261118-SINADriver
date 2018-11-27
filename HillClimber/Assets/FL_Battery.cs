0744b6bc525ef2b53441f1c8d12a8b94df50bfd5
Ferri de Lange <ev.fdl.ve@gmail.com>
2018-11-27 11:41:59 +0100

    Battery prefab

---
+++ HillClimber/Assets/FL_Battery.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FL_Battery : MonoBehaviour {
+	
+    public float Capacity {
+        get; private set;
+    }
+
+    private void Start() {
+        Capacity = 10;
+    }
+
+}
