ec7a925f466b8affa8f149045aec879eafa269de
Tommiee <tomdepom1999@gmail.com>
2018-11-29 11:47:43 +0100

    Collider script
    Script calls raycast and checks for ground

---
+++ HillClimber/Assets/Scripts/tw/tw_GameOver.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class tw_GameOver : MonoBehaviour {
+
+	[SerializeField]
+	LayerMask layerMask;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.TransformDirection(Vector2.up), 5, layerMask);
+
+		Debug.DrawRay (transform.position, transform.TransformDirection(Vector2.up) * 5);
+
+		if (hit.collider != null ) {
+			print ("hit something");
+			print (hit.collider);
+		}
+	}
+}
