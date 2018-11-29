a13963a680ee8fdea0967c516f2c544e5a888fb3
AlexJanson <alex@hoofddorp.nl>
2018-11-29 11:06:36 +0100

    BatteryAmount.cs now holds batteryAmount variable

--- HillClimber/Assets/Scripts/aj/BatteryAmount.cs
+++ HillClimber/Assets/Scripts/aj/BatteryAmount.cs
@@ -13,18 +13,20 @@ public class BatteryAmount : MonoBehaviour {
 	public float decreaseAmount = 0.01f;
 	public float timeToDecrease = 0.1f;
 	public float batteryAmount = 100f;
+	private float batteryAmountMax;
 
 	public delegate void FuelIsEmpty();
 	public event FuelIsEmpty fuelIsEmptyEvent;
 
 	void Start() {
+		batteryAmountMax = batteryAmount;
 		slider = GetComponent<Slider>();
-		sliderValue = batteryAmount / 100;
+		sliderValue = batteryAmount / batteryAmountMax;
 		StartCoroutine(DecreaseBatteryAmount());
 	}
 
 	void Update() {
-		slider.value = sliderValue;
+		slider.value = sliderValue / 100;
 		sliderImage.color = Color.Lerp(Color.red, Color.white, sliderValue);
 	}
 
@@ -37,12 +39,22 @@ public class BatteryAmount : MonoBehaviour {
 		yield return null;
 	}
 
-	private void CallEvent() {
-		if(fuelIsEmptyEvent != null)
-			fuelIsEmptyEvent();
+	public void ChangeSliderValue(float value) {
+		batteryAmount -= batteryAmount / 100 - value;
+		sliderValue = batteryAmount;
 	}
 
-	public void ChangeSliderValue(float value) {
-		sliderValue -= value;
+	private void CallEvent() {
+		if(fuelIsEmptyEvent != null) {
+			fuelIsEmptyEvent();
+		}
+	}
+
+	public void AddAmount(float value) {
+		batteryAmount += value;
+		if(batteryAmount > 100f) {
+			batteryAmount = 100f;
+		}
+		sliderValue = batteryAmount;
 	}
 }
