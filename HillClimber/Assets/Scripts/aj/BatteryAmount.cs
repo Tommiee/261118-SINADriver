12805f0c0cd19926cdec5fa298aa4860fa896ade
Wesley Cats <23973@ma-web.nl>
2018-11-29 10:14:30 +0100

    Merged Battery UI + decrease over time into Develop

---
+++ HillClimber/Assets/Scripts/aj/BatteryAmount.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+public class BatteryAmount : MonoBehaviour {
+
+	Slider slider;
+	float sliderValue;
+
+	public Image sliderImage;
+	public float decreaseAmount = 0.01f;
+	public float timeToDecrease = 0.1f;
+
+	public delegate void FuelIsEmpty();
+	public event FuelIsEmpty fuelIsEmptyEvent;
+
+	void Start() {
+		slider = GetComponent<Slider>();
+		sliderValue = slider.value;
+		StartCoroutine(DecreaseBatteryAmount());
+	}
+
+	void Update() {
+		slider.value = sliderValue;
+		sliderImage.color = Color.Lerp(Color.red, Color.white, sliderValue);
+	}
+
+	IEnumerator DecreaseBatteryAmount() {
+		while(sliderValue > 0) {
+			ChangeSliderValue(decreaseAmount);
+			yield return new WaitForSeconds(timeToDecrease);
+		}
+		CallEvent();
+		yield return null;
+	}
+
+	private void CallEvent() {
+		if(fuelIsEmptyEvent != null)
+			fuelIsEmptyEvent();
+	}
+
+	public void ChangeSliderValue(float value) {
+		sliderValue -= value;
+	}
+}
