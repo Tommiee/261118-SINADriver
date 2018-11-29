3e7ec1dbf08ac75d274482a4cfd09937538b22d6
AlexJanson <alex@hoofddorp.nl>
2018-11-29 10:07:33 +0100

    gitignore ignores vscode/ directory, battery collision bus

---
+++ HillClimber/Assets/Scripts/aj/aj_BusCollision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class aj_BusCollision : MonoBehaviour {
+
+	BoxCollider2D collider;
+
+	void Start() {
+		collider = GetComponent<BoxCollider2D>();
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Battery") {
+			//Battery has collided with the bus!
+			print("Battery collision!");
+			Destroy(col.gameObject);
+		}
+    }
+}
