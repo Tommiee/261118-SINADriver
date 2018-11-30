00b4d33768c0837349adefd91544fedb01c9f51e
Ferri de Lange <ev.fdl.ve@gmail.com>
2018-11-30 15:04:47 +0100

    Added script to decrease battery while driving

---
+++ HillClimber/Assets/Scripts/fl/fl_DriveBatteryDecrease.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fl_DriveBatteryDecrease : MonoBehaviour {
+
+    [SerializeField] private float decreaseBy = .1f;
+
+    [SerializeField] private BatteryAmount batteryAmount;
+    private tw_InputManager tw_InputManager;
+
+	void Start () {
+        tw_InputManager = GetComponent<tw_InputManager>();
+    }
+	
+	void Update () {
+        if(tw_InputManager.Up()) {
+            batteryAmount.AddAmount(-decreaseBy);
+        }
+	}
+}
