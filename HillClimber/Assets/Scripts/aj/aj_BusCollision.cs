6dcfe166eb975ab0b217a940620651975aa77687
jamirodevulder <31829657+jamirodevulder@users.noreply.github.com>
2018-12-03 16:01:19 +0100

    added money script + ui - works with collision

--- HillClimber/Assets/Scripts/aj/aj_BusCollision.cs
+++ HillClimber/Assets/Scripts/aj/aj_BusCollision.cs
@@ -8,10 +8,11 @@ public class aj_BusCollision : MonoBehaviour {
 	BoxCollider2D collider;
 	
 	BatteryAmount batteryAmount;
-
+    jv_moneycount moneycount;
 	void Start() {
 		collider = GetComponent<BoxCollider2D>();
 		batteryAmount = FindObjectOfType<BatteryAmount>();
+        moneycount = FindObjectOfType<jv_moneycount>();
 	}
 
 	void OnTriggerEnter2D(Collider2D col)
@@ -20,5 +21,11 @@ public class aj_BusCollision : MonoBehaviour {
 			batteryAmount.AddAmount(25f);
 			Destroy(col.gameObject);
 		}
+        if (col.gameObject.tag == "Homeless")
+        {
+            moneycount.AddMoney(5);
+            Destroy(col.gameObject);
+        }
     }
+
 }
---
+++ HillClimber/Assets/Scripts/jv/jv_moneycount.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class jv_moneycount : MonoBehaviour {
+    public TextMeshProUGUI money;
+    public int MoneyAmount
+    {
+        get{return moneyAmount;}
+        set { moneyAmount = value; }
+    }
+    private int moneyAmount;
+    private string zero = ": 0";
+
+    /*
+     * public var GetterSetterVar { get; set; }
+     * 
+     * private var privateVarForGetterSetter;
+     * public var AlsoGetterSetter {
+     *      get {
+     *          return privateVarForGetterSetter;
+     *      }
+     *      set {
+     *          privateVarForGetterSetter = value; // value will automatically be determined by the getter/setter
+     *      }
+     * }
+     */
+    
+	// Use this for initialization
+
+    void Start()
+    {
+        
+        DontDestroyOnLoad(this.gameObject);
+        
+    }
+    
+    public void AddMoney(int value)
+    {
+        moneyAmount += value;
+        if(moneyAmount >=  10)
+        {
+            zero = ": ";
+        }
+        money.text = zero + moneyAmount;
+
+    }
+
+}
