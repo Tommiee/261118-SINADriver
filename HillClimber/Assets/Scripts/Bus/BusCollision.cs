837a11fe725148e8cbbafa8eb957bd7397294e67
Wesley Cats <23973@ma-web.nl>
2018-12-07 12:42:41 +0100

    Renamed bus collision script

---
+++ HillClimber/Assets/Scripts/Bus/BusCollision.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class BusCollision : MonoBehaviour {
+
+	BoxCollider2D collider;
+	GameManager gameManager;
+	Data data;
+
+
+	BatteryAmount battery;
+	[SerializeField] private LayerMask layerMask;
+
+	void Start() {
+		collider = GetComponent<BoxCollider2D>();
+		battery = FindObjectOfType<BatteryAmount>();
+		gameManager = FindObjectOfType<GameManager>();
+		data = FindObjectOfType<Data>();
+	}
+
+	private void Update()
+	{
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.TransformDirection(Vector2.up), 5, layerMask);
+
+		//Debug.DrawRay(transform.position, transform.TransformDirection(Vector2.up) * 5);
+
+		if (hit.collider != null)
+		{
+			gameManager.Stop(this.gameObject);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Battery") {
+			battery.batteryAmount = battery.maxBatteryAmount;
+			Destroy(col.gameObject);
+		}
+
+		if (col.gameObject.tag == "Poor")
+		{
+			data.AddMoney(5);
+			Destroy(col.gameObject);
+		}
+	}
+}
