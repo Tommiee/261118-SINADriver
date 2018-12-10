a56246826e8b90cf25c6cedf0ccf7740b7aad9c3
jamirodevulder <31829657+jamirodevulder@users.noreply.github.com>
2018-12-10 09:35:20 +0100

    Preparation Upgrade menu

---
+++ HillClimber/Assets/Scripts/jv/jv_UpgradeMenuScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jv_UpgradeMenuScript : MonoBehaviour {
+    public UpgradeData upgradeData;
+    
+	// Use this for initialization
+	void Start () {
+
+        upgradeData.motorLevel = 1;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void Reset()
+    {
+        upgradeData.motorLevel = 0;
+        upgradeData.gripLevel = 0;
+        upgradeData.batteryLevel = 0;
+        upgradeData.boostLevel = false;
+        upgradeData.currencyLevel = 0;
+        upgradeData.solarPanelsLevel = false;
+}
+}
